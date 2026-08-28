4c2c74fcc6ff6d6ced514f2cd71ae6f341c8fbc1
eduar-silva <eduarbbrito@gmail.com>
2026-08-28 17:02:55 -0300

    commit aula143 exercicio polimorrfismo

---
+++ main-workspace/aula143Exercicio/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> employees = new ArrayList<>();
+		
+		System.out.print("Enter the number of employees: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("Employee #" + i + " data: ");
+			System.out.print("Outsourced (y/n)? ");
+			String answer = sc.next();
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Hours: ");
+			Integer hours = sc.nextInt();
+			System.out.print("Value per hour: ");
+			Double valuePerHour = sc.nextDouble();
+			
+			if(answer.equalsIgnoreCase("y")) {
+				System.out.print("Additional charge: ");
+				Double additionalCharge = sc.nextDouble();
+				Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				employees.add(emp);
+			} 
+			else {
+				Employee emp = new Employee(name, hours, valuePerHour);
+				employees.add(emp);
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Payments: ");
+		for(Employee e : employees) {
+			System.out.println(e);
+		}
+		
+		sc.close();	
+	}
+}
